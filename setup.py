f8e14340b7dfafcd05d5de73a0fc3cc977f676f5
M4chin3M4N <75846902+M4chin3M4N@users.noreply.github.com>
2021-04-01 11:07:14 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+if os.getuid != 0:
+    print("[!]YouMustExecute as root!!!!")
+    sys.exit()
+
+print("[+]Now SetUp....")
+
+step1 = "chmod +x bdoor-gen.py"
+
+step2 = "sudo mv bdoor-gen.py /usr/bin/bdoor-gen"
+
+os.system(step1)
+
+os.system(step2)
+
+print("Done...")
